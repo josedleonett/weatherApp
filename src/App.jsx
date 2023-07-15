0d4ed78f7b94abb61faf93fd090a4f761d14336c
JosedLeonett <josed.95@gmail.com>
2023-07-15 10:39:34 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { Icon } from "./components/atoms/icon/Icon";
+import { WiDayCloudyGusts } from "react-icons/wi";
 
 function App() {
 
   return (
     <>
-      
+    <p>hola</p>
+    <WiDayCloudyGusts color='red'/>
+      <Icon>
+        <WiDayCloudyGusts/>
+      </Icon>
     </>
   )
 }
