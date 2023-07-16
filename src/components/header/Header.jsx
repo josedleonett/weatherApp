8627fa5a502ee16f2675e3a5a0d8d2ca076c1d2e
JosedLeonett <josed.95@gmail.com>
2023-07-15 22:07:00 -0300

    primer commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Searchbar from '../searchbar/Searchbar';
+import Navbar from '../navbar/Navbar';
+
+const Header = () => {
+  return (
+    <>    
+      <Searchbar />
+      <Navbar />
+    </>
+  );
+}
+
+export default Header
